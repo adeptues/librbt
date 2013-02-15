99aaf0233c67ef5251c0b90fdf5096a8f493d7aa
Thomas Helmkay <adeptues@googlemail.com>
2013-02-15 22:05:42 +0000

    added findrobotarm function

---
+++ armfunc.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void moveMotor();
+
+void closeClaw();
+
+void openClaw();
--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 #include <libusb.h>
 
+//defines
+#define id  4711
+#define part  0000
+
 /**
  * This is designed to be a user mode library for controlling a robot arm
  * */
@@ -25,23 +29,66 @@ static void print_devs(libusb_device **devs)
 	}
 }
 
+
+///Should find the robot arm device on the system and return a pointer to that device
+libusb_device * findRobotArm(libusb_device **devs,libusb_device_handle ** devh){
+  libusb_device * devPtr;
+  
+  int count = 0;
+  while((devPtr = devs[count++]) != NULL){
+    struct libusb_device_descriptor descr;
+    int rtnCode = libusb_get_device_descriptor(devPtr,&descr);
+
+    if(rtnCode < 0){
+      printf("Failed to get device descriptor\n");
+      return NULL;
+    }
+
+    if((descr.idVendor == id )&& (descr.idProduct == part)){
+      printf("Found Robot Arm Device!\n");
+      libusb_open(devPtr,);
+      return devPtr;
+    }
+
+    
+    
+  }
+
+  printf("could not find device robot arm with %d : %d ",id,part);
+  return NULL;
+
+  
+  
+  
+}
+
 int main(void)
 {
 	libusb_device **devs;
+	//libusb_device_handle ** devh;
 	int r;
 	ssize_t cnt;
 
-	r = libusb_init(NULL);
+	
+	r = libusb_init(NULL); // must be called fir st before any other libusb function 
 	if (r < 0)
-		return r;
+		return r;//failed to initialise
 
-	cnt = libusb_get_device_list(NULL, &devs);
-	if (cnt < 0)
+	cnt = libusb_get_device_list(NULL, &devs);//get a list of all usb devices on the system place it into the collection struct libusb_device
+	if (cnt < 0)//cnt number of returned devices 
 		return (int) cnt;
 
-	print_devs(devs);
-	libusb_free_device_list(devs, 1);
+	//print_devs(devs);
+	libusb_device * dev =  findRobotArm(devs);
 
-	libusb_exit(NULL);
+	if(dev != NULL){
+	  printf("find robot arm worksa\n");
+	}
+
+	//next step open device / claim interface and send data then free
+	
+	libusb_free_device_list(devs, 1);//free device list
+
+	libusb_exit(NULL);//must be called when program quits
 	return 0;
 }
